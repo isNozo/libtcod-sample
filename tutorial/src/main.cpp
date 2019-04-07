fdbd021db24ba0113d394991a73dc721447b26ae
nozo <33536862+isNozo@users.noreply.github.com>
2019-04-07 20:52:13 +0900

    Convert C++ code to C

--- tutorial/src/main.cpp
+++ tutorial/src/main.cpp
@@ -1,12 +1,12 @@
-#include "libtcod.hpp"
+#include "libtcod.h"
 int main() {
-   TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
-   while ( !TCODConsole::isWindowClosed() ) {
+   TCOD_console_init_root(80,50,"libtcod C++ tutorial",false,TCOD_RENDERER_OPENGL);
+   while ( !TCOD_console_is_window_closed() ) {
        TCOD_key_t key;
-       TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
-       TCODConsole::root->clear();
-       TCODConsole::root->putChar(40,25,'@');
-       TCODConsole::flush();
+       TCOD_sys_check_for_event(TCOD_EVENT_KEY_PRESS,&key,NULL);
+       TCOD_console_clear(NULL);
+       TCOD_console_put_char(NULL, 40,25,'@',TCOD_BKGND_DEFAULT);
+       TCOD_console_flush();
    }
    return 0;
 }
