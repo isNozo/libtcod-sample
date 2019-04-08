90c1806e7705a92628dfa70ff18c16ec8ad6e406
nozo <33536862+isNozo@users.noreply.github.com>
2019-04-08 21:23:27 +0900

    Add print function to write multi letter

--- tutorial/src/main.cpp
+++ tutorial/src/main.cpp
@@ -1,8 +1,18 @@
 #include "libtcod.h"
+
+#define Win_W 60
+#define Win_H 30
+
+void print_message(int x, int y, wchar_t *str) {
+    for(;*str!='\0';str++)
+        TCOD_console_put_char(NULL,x++,y,*str,TCOD_BKGND_DEFAULT);
+}
+
 int main() {
-    int playerx=40,playery=25;
+    int playerx=0,playery=0;
+
     TCOD_console_set_custom_font("font.png",TCOD_FONT_LAYOUT_ASCII_INROW,32,2048);
-    TCOD_console_init_root(80,50,"libtcod C++ tutorial",false,TCOD_RENDERER_SDL);
+    TCOD_console_init_root(Win_W,Win_H,"libtcod C++ tutorial",false,TCOD_RENDERER_SDL);
     while ( !TCOD_console_is_window_closed() ) {
         TCOD_key_t key;
         TCOD_sys_check_for_event(TCOD_EVENT_KEY_PRESS,&key,NULL);
@@ -16,7 +26,7 @@ int main() {
        }
 
         TCOD_console_clear(NULL);
-        TCOD_console_put_char(NULL,playerx,playery,L'あ',TCOD_BKGND_DEFAULT);
+        print_message(playerx, playery, L"あいうえお");
         TCOD_console_flush();
     }
     return 0;
