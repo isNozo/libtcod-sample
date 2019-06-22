d2baf11a00209af11ae00a8c8f63269406fcc167
nozo <33536862+isNozo@users.noreply.github.com>
2019-06-22 22:49:15 +0900

    Remove gettext-test. Add comment to libtcod-tutorial.

--- tutorial/src/main.c
+++ tutorial/src/main.c
@@ -1,3 +1,8 @@
+/*
+ * Complete roguelike tutorial using C++ and libtcod : 
+ * http://www.roguebasin.com/index.php?title=Complete_roguelike_tutorial_using_C%2B%2B_and_libtcod_-_part_1:_setting_up
+ */
+
 #include "libtcod.h"
 
 #define Win_W 60
